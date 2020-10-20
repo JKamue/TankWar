ea873c68e14947f5e55f32e097f4fa3aa19615af
JKamue <mail@jkamue.de>
2020-10-20 13:54:24 +0200

    Set first simple map

--- TankWar/TankWarServer/Program.cs
+++ TankWar/TankWarServer/Program.cs
@@ -12,10 +12,23 @@ namespace TankWarServer
     {
         static void Main(string[] args)
         {
+            var lines = new List<Line>
+            {
+                // Spawn Area left
+                new Line(Color.DarkGreen, new PointF(60,30), new PointF(160,90)),
+                new Line(Color.DarkGreen, new PointF(60,270), new PointF(160,210)),
+                new Line(Color.DarkGreen, new PointF(200,120), new PointF(200,180)),
+                new Line(Color.DarkGreen, new PointF(260,80), new PointF(260,120)),
+                new Line(Color.DarkGreen, new PointF(260,220), new PointF(260,180)),
 
-            var line1 = new Line(Color.Green, new PointF(10,10), new PointF(20,80));
-            var lines = new List<Line> {line1};
-            var map = new Map(lines, new Size(200,200));
+                // Spawn Area right
+                new Line(Color.DarkRed, new PointF(540,30), new PointF(440,90)),
+                new Line(Color.DarkRed, new PointF(540,270), new PointF(440,210)),
+                new Line(Color.DarkRed, new PointF(400,120), new PointF(400,180)),
+                new Line(Color.DarkRed, new PointF(340,80), new PointF(340,120)),
+                new Line(Color.DarkRed, new PointF(340,220), new PointF(340,180))
+            };
+            var map = new Map(lines, new Size(600,300));
 
             var server = new TankWarLib.TankWarServer(map, 50000, true);
             Console.WriteLine("Server started\n--------------");
